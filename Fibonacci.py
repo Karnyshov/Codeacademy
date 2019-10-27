165746f8eccbf95c21f3f6cf20989419360a5932
kyrylo.karnyshov@gmail.com <Hub0ctObus1>
2019-10-27 22:43:55 +0200

    Small fix

--- Fibonacci.py
+++ Fibonacci.py
@@ -4,7 +4,7 @@ def fibonacci(x):
         return "Incorrect input"
     elif x == 0:
         return 0
-    elif x == 1 or x == 1:
+    elif x == 1 or x == 2:
         return 1
     else:
         return fibonacci(x-1) + fibonacci(x-2)
